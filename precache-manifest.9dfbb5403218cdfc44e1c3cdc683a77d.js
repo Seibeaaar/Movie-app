9117589dedde383d325c602a21d6591b0524ef69
Seibeaaar <yuribar4520@gmail.com>
2020-06-13 14:50:21 +0300

    Updates

---
+++ precache-manifest.9dfbb5403218cdfc44e1c3cdc683a77d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb22ca54a9ae3a878dbab4a5fbf22d3f",
+    "url": "/Movie-app/index.html"
+  },
+  {
+    "revision": "142b9c2db4892ee37685",
+    "url": "/Movie-app/static/css/main.9f29dd14.chunk.css"
+  },
+  {
+    "revision": "742c904a93d5ad916458",
+    "url": "/Movie-app/static/js/2.90b920de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-app/static/js/2.90b920de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142b9c2db4892ee37685",
+    "url": "/Movie-app/static/js/main.ec88d86d.chunk.js"
+  },
+  {
+    "revision": "857d2128c8e1c397c64b",
+    "url": "/Movie-app/static/js/runtime-main.977928ac.js"
+  },
+  {
+    "revision": "5a71fa48ae1965c00f977c5aeb436b57",
+    "url": "/Movie-app/static/media/NotFound.5a71fa48.png"
+  }
+]);
